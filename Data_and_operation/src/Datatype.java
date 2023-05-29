776064b2a9e89a7dbc6ffd520f5a79337d2315bd
8luerose <8luerose@naver.com>
2023-05-30 05:11:49 +0900

    1_2 Datatype (+ 'H'char type)

--- Data_and_operation/src/Datatype.java
+++ Data_and_operation/src/Datatype.java
@@ -6,5 +6,6 @@ public class Datatype {
         System.out.println(6+6);        //12
         System.out.println("6" + "6");  //66
         System.out.println("1234".length());    //문자열.length() = 문자열의 길이
+        System.out.println('H');
     }
 }
