cffae7c53eee57999f93d664678e901bd5666e6e
8luerose <8luerose@naver.com>
2023-05-30 05:10:16 +0900

    1_2 Datatype

---
+++ Data_and_operation/src/Datatype.java
@@ -0,0 +1,10 @@
+public class Datatype {
+    public static void main(String[] args){
+        System.out.println(6);          //Number
+        System.out.println("six");      //String
+        System.out.println("6");        //String
+        System.out.println(6+6);        //12
+        System.out.println("6" + "6");  //66
+        System.out.println("1234".length());    //문자열.length() = 문자열의 길이
+    }
+}
