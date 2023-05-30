4759ed698c5c30a7b80766c33a9b36dc2b7fd06b
8luerose <8luerose@naver.com>
2023-05-31 05:07:59 +0900

    1_3 Number

---
+++ Data_and_operation/src/Number.java
@@ -0,0 +1,18 @@
+public class Number {
+    public static void main(String[] args) {
+        //  Operator
+        System.out.println(6 + 2);
+        System.out.println(6 - 2);
+        System.out.println(6 * 2);
+        System.out.println(6 / 2);
+        System.out.println(Math.PI);
+        System.out.println(Math.floor(Math.PI));
+        System.out.println(Math.ceil(Math.PI));
+    }
+}
+
+//개념
+//1.sout = System.out.println( ); 생성해줌
+//2.Math. = 관련된 기능 제공 ex)PI
+//3.Math.floor() = 소수점 0으로 초기화
+//4.Math.ceil() = 소수점있으면 무조건 올림 ex)3.1 -> 4.0
