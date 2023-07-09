9af02eb7b4eebdc6000979a29b31ce2e336797bb
8luerose <8luerose@naver.com>
2023-07-10 00:53:29 +0900

    ~Chater MyAPP_2 (a comprehensive concept about java)

---
+++ MyApp/src/AccountingArrayApp.java
@@ -0,0 +1,45 @@
+public class AccountingArrayApp {
+
+    public static void main(String[] args) {
+
+        double valueOfSupply = Double.parseDouble(args[0]);
+        double vatRate = 0.1;
+        double expenseRate = 0.3;
+        double vat = valueOfSupply * vatRate;
+        double total = valueOfSupply + vat;
+        double expense = valueOfSupply * expenseRate;
+        double income = valueOfSupply - expense;
+
+//        double[] dividendRates = new double[3];
+//        dividendRates[0] = 0.5;
+//        dividendRates[1] = 0.3;
+//        dividendRates[2] = 0.2;
+
+        double[] dividendRates = new double[3];
+        //double[] 배열 생성 + 변수(그릇) = new 생성할 그릇[배열 갯수]
+        dividendRates[0] = 0.5;
+        dividendRates[1] = 0.3;
+        dividendRates[2] = 0.2;
+//        double dividend1 = income * dividendRates[0];
+//        double dividend2 = income * dividendRates[1];
+//        double dividend3 = income * dividendRates[2];
+
+        int i = 0;
+        while (i < dividendRates.length)
+        {
+            System.out.println("Dividend" + (i+1) + ": " + (income * dividendRates[i]));
+            i += 1;
+        }
+
+        System.out.println("Value of supply : " + valueOfSupply);
+        System.out.println("VAT : " + vat);
+        System.out.println("Total : " + total);
+        System.out.println("Expense : " + expense);
+        System.out.println("Income : " + income);
+//        System.out.println("Dividend 1 : " + dividend1);
+//        System.out.println("Dividend 2 : " + dividend2);
+//        System.out.println("Dividend 3 : " + dividend3);
+
+    }
+
+}
